81c3f8a192bf02924c0df6a648dec46fd2dec64c
TordStavik <tord.stavik@inmeta.no>
2019-01-07 13:49:30 +0100

    Working from example code. Not satisfactory image recognition

---
+++ PoC_Tesseract/PoC_Tesseract/ImageHandler.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace PoC_Tesseract
+{
+    public class ImageHandler
+    {
+        private static bool again = true;
+        private VideoDeviceHandler videoDeviceHandler = new VideoDeviceHandler();
+        public ImageHandler()
+        {
+            Start();
+        }
+        private void Start()
+        {
+            while (again)
+            {
+                Console.Clear();
+                DecideImageProcessing();
+            }
+        }
+
+        public async void DecideImageProcessing()
+        {
+            Console.WriteLine("Press 0 for image processing using camera or 1-9 for stored images");
+            char userInput = Console.ReadKey().KeyChar;
+            Console.WriteLine("\n");
+            if (userInput == '0')
+            {
+                videoDeviceHandler.FindCapturingDevice();
+            }
+            else if (char.IsDigit(userInput))
+            {
+                try
+                {
+                    string path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\images\" + userInput + ".jpg";
+
+                    if (File.Exists(path))
+                    {
+                        ReadImage(path);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nInvalid file path");
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+
+            }
+            else
+            {
+                again = false;
+            }
+        }
+
+       
+
+        public static void ReadImage(string path)
+        {
+            try
+            {
+                using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(path))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var text = page.GetText();
+                            Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence());
+
+                            Console.WriteLine("Text (GetText): \r\n{0}", text);
+                            Console.WriteLine("Text (iterator):");
+                            using (var iter = page.GetIterator())
+                            {
+                                iter.Begin();
+
+                                do
+                                {
+                                    do
+                                    {
+                                        do
+                                        {
+                                            do
+                                            {
+                                                if (iter.IsAtBeginningOf(PageIteratorLevel.Block))
+                                                {
+                                                    Console.WriteLine("<BLOCK>");
+                                                }
+
+                                                Console.Write(iter.GetText(PageIteratorLevel.Word));
+                                                Console.Write(" ");
+
+                                                if (iter.IsAtFinalOf(PageIteratorLevel.TextLine, PageIteratorLevel.Word))
+                                                {
+                                                    Console.WriteLine();
+                                                }
+                                            } while (iter.Next(PageIteratorLevel.TextLine, PageIteratorLevel.Word));
+
+                                            if (iter.IsAtFinalOf(PageIteratorLevel.Para, PageIteratorLevel.TextLine))
+                                            {
+                                                Console.WriteLine();
+                                            }
+                                        } while (iter.Next(PageIteratorLevel.Para, PageIteratorLevel.TextLine));
+                                    } while (iter.Next(PageIteratorLevel.Block, PageIteratorLevel.Para));
+                                } while (iter.Next(PageIteratorLevel.Block));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError(e.ToString());
+                Console.WriteLine("Unexpected Error: " + e.Message);
+                Console.WriteLine("Details: ");
+                Console.WriteLine(e.ToString());
+            }
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+
+
+}
---
+++ PoC_Tesseract/PoC_Tesseract/VideoDeviceHandler.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace PoC_Tesseract
+{
+    public class VideoDeviceHandler
+    {
+        private static FilterInfoCollection CaptureDevice;
+        private static VideoCaptureDevice FinalFrame;
+
+        private static Stopwatch stopwatch = new Stopwatch();
+        private static int selectedDevice;
+
+        public VideoDeviceHandler()
+        {
+
+        }
+
+        public void FindCapturingDevice()
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);//constructor
+            if (CaptureDevice.Count > 0)
+            {
+                FinalFrame = new VideoCaptureDevice();
+                SelectCapturingDevice();
+            }
+            else
+            {
+                Console.WriteLine("You do not have a device for video capture");
+            }
+        }
+
+        public void SelectCapturingDevice()
+        {
+            if (CaptureDevice.Count == 1)
+            {
+                selectedDevice = 1;
+                Console.WriteLine("\nUsing " + CaptureDevice[0].Name + "for capturing image");
+                SaveImage();
+            }
+            else
+            {
+                Console.WriteLine("Please select your desired camera");
+                int count = 1;
+                foreach (FilterInfo cam in CaptureDevice)
+                {
+                    Console.WriteLine($"{count}. {cam.Name}");
+                    count++;
+                }
+
+
+                char userInput = Console.ReadKey().KeyChar;
+
+                if (char.IsDigit(userInput))
+                {
+                    selectedDevice = (int)char.GetNumericValue(userInput);
+                    SaveImage();
+                }
+            }
+        }
+        private void SaveImage()
+        {
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[selectedDevice - 1].MonikerString);
+            FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+            FinalFrame.Start();
+        }
+
+        private async void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            var image = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap varBmp = new Bitmap(image);
+
+            varBmp.Save(GetNextFileName(), ImageFormat.Png);
+            Console.WriteLine($"\nSaving photo");
+            varBmp.Dispose();
+            varBmp = null;
+            stopwatch.Restart();
+            var dir = GetDirectory();
+            DirectoryInfo directory = new DirectoryInfo(dir);
+            var filePairs = directory
+                .GetFiles("*.png")
+                .OrderByDescending(f => f.CreationTime);
+            ImageHandler.ReadImage(filePairs.First().FullName);
+            FinalFrame.SignalToStop();
+        }
+
+        private static string GetNextFileName()
+        {
+            string path = GetDirectory();
+            bool exists = System.IO.Directory.Exists(path);
+
+            if (!exists)
+            {
+                var directory = Directory.CreateDirectory(path);
+            }
+            int fCount = Directory.GetFiles(path, "*", SearchOption.AllDirectories).Length;
+
+            var imgPath = path + @"\capture" + fCount + ".png";
+            return imgPath;
+        }
+        private static string GetDirectory()
+        {
+            return System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\images";
+        }
+    }
+}
